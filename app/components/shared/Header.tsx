d4b0494648593a54c5afcb057ba5640c809812c5
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-24 12:58:37 +0200

    Co-authored-by: Stefan Airoaie <StefanAiroaie@users.noreply.github.com>

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -1,18 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <nav className="flex justify-between items-center p-2">
-      <h1>
-        Eventful<span className="text-primary">App</span>
-      </h1>
-      <div className="flex gap-2">
-        <Button>Login</Button>
-        <Button>Register</Button>
-        <ModeToggle />
-      </div>
-    </nav>
+    <header className="w-full">
+      <nav className="wrapper flex justify-between items-center">
+        <Link href={"/"} className="flex gap-1 w-36 items-center">
+          <Image
+            src={"/assets/images/logo.svg"}
+            alt="Eventful Logo"
+            width={32}
+            height={32}
+          />
+          <h1>
+            Eventful<span className="text-primary">App</span>
+          </h1>
+        </Link>
+
+        <div className="flex gap-2">
+          <Button>Login</Button>
+          <Button>Register</Button>
+          <ModeToggle />
+        </div>
+      </nav>
+    </header>
   );
 };
 
