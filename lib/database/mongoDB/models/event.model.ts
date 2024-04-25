27d278be4640f01ab7f882d5b0ea52fe09a36221
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-25 15:28:15 +0200

    clerk webhook

---
+++ lib/database/mongoDB/models/event.model.ts
@@ -0,0 +1,79 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description: string;
+  location: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: number;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
+  attendees: string[];
+}
+
+const EventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: {
+    type: String,
+    requiered: true,
+  },
+  startDateTime: {
+    type: Date,
+    requiered: true,
+  },
+  endDateTime: {
+    type: Date,
+    requiered: true,
+  },
+  price: {
+    type: Number,
+    requiered: true,
+  },
+  isFree: {
+    type: Boolean,
+    default: false,
+  },
+  url: {
+    type: String,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  organizer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  attendees: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    default: [],
+  },
+});
+
+const Event = models.Event || model("Event", EventSchema, "events");
+
+export default Event;
