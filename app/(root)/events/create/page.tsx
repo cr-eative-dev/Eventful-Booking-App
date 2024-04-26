e8d63c58fde156f33aa5fb4ebc69087b7bfb3d4c
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-04-26 10:32:51 +0200

    stefan 1. update

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const CreateEvent = () => {
   return (
     <section>
-      <h3 className="wrapper h3-bold text-left sm:text-center">Create Event</h3>
+      <h3 className="wrapper h3-bold text-left sm:text-center">Create Event!</h3>
+      <h3 className="wrapper h3-bold text-left sm:text-center">Create Event!</h3>
+
     </section>
   );
 };
