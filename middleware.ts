78094a44a6fcce449141ded7c89db943ee518529
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-26 16:19:03 +0200

    protect events create

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/events/create(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
