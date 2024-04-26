524609c403b523e7d3a883dc367ed23702b30688
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-26 10:27:20 +0200

    Co-authored-by: Stefan Airoaie <StefanAiroaie@users.noreply.github.com>

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateEvent = () => {
+  return (
+    <section>
+      <h3 className="wrapper h3-bold text-left sm:text-center">Create Event</h3>
+    </section>
+  );
+};
+
+export default CreateEvent;
