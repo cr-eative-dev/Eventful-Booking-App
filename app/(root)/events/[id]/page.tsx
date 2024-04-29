436ca1ef3a14d98ea85c6fcb5cd6c0a154c6d951
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-04-29 16:38:17 +0200

    some updates on eventpge

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -7,6 +7,12 @@ import {
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
+import {
+  CircleDollarSign,
+  CalendarCheck2,
+  CalendarX2,
+  MapPin,
+} from "lucide-react";
 
 const EventDetails = async ({
   params: { id },
@@ -38,7 +44,7 @@ const EventDetails = async ({
 
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
-                  <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  <p className="p-bold-20 rounded-full bg-gray-500/10 px-5 py-2 text-gray-700">
                     {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
                   <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
@@ -46,12 +52,12 @@ const EventDetails = async ({
                   </p>
                 </div>
 
-                <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
-                  by{" "}
+                {/* <p className="p-medium-16 ml-2 mt-2 sm:mt-0">
+                  Event added by{" "}
                   <span className="text-primary-500">
                     {event.organizer.firstName} {event.organizer.lastName}
                   </span>
-                </p>
+                </p> */}
               </div>
             </div>
 
@@ -59,12 +65,7 @@ const EventDetails = async ({
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
-                <Image
-                  src="/assets/icons/calendar.svg"
-                  alt="calendar"
-                  width={32}
-                  height={32}
-                />
+                <CalendarCheck2 />
                 <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                   <p>
                     {formatDateTime(event.startDateTime).dateOnly} -{" "}
@@ -78,23 +79,27 @@ const EventDetails = async ({
               </div>
 
               <div className="p-regular-20 flex items-center gap-3">
-                <Image
-                  src="/assets/icons/location.svg"
-                  alt="location"
-                  width={32}
-                  height={32}
-                />
+                <MapPin />
                 <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
               <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
-              <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
-              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+              <p className="p-regular-16 lg:p-regular-18">
+                {event.description}
+              </p>
+              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline cursor-pointer">
                 {event.url}
               </p>
             </div>
+
+            <p className="p-medium-14 italic text-left mt-2 sm:mt-0">
+              Event added by{" "}
+              <span className="text-primary-500">
+                {event.organizer.firstName} {event.organizer.lastName}
+              </span>
+            </p>
           </div>
         </div>
       </section>
