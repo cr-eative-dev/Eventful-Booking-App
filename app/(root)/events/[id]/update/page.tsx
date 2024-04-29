5880e3dc50e18226f2675897f882ba41098e40c9
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-29 11:17:06 +0200

    update

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,20 +1,34 @@
 import EventForm from "@/app/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs/server";
-import * as z from "zod";
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
+
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id);
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover py-10 bg-center md:py-5">
-        <h3 className="wrapper h3-bold text-left sm:text-center">
-          Update Event!
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update Event
         </h3>
       </section>
+
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event._id}
+          userId={userId}
+        />
       </div>
     </>
   );
