acbc25d0510149096ab648df698f0b8f7ef63635
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-29 10:01:10 +0200

    form

--- app/components/shared/EventForm.tsx
+++ app/components/shared/EventForm.tsx
@@ -28,25 +28,85 @@ import { DatePicker } from "./DatePicker";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Link } from "lucide-react";
 import { CircleDollarSign } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useUploadThing } from "@/lib/uploadthing";
+import { createEvent, updateEvent } from "@/lib/actions/event.actions";
 
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
 };
 const EventForm = ({ userId, type }: EventFormProps) => {
-  const initialValues = eventDefaultValues;
   const [files, setFiles] = useState<File[]>([]);
+
+  const initialValues =
+    event && type === "Update"
+      ? {
+          ...event,
+          startDateTime: new Date(event.startDateTime),
+          endDateTime: new Date(event.endDateTime),
+        }
+      : eventDefaultValues;
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing("imageUploader");
 
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if (files.length > 0) {
+      const uploadedImages = await startUpload(files);
+
+      if (!uploadedImages) {
+        return;
+      }
+
+      uploadedImageUrl = uploadedImages[0].url;
+    }
+
+    if (type === "Create") {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: "/profile",
+        });
+
+        if (newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (type === "Update") {
+      if (!eventId) {
+        router.back();
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`,
+        });
+
+        if (updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   return (
@@ -264,7 +324,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               type="submit"
               size="lg"
               disabled={form.formState.isSubmitting}
-              className="button col-span-2 w-full"
+              className="w-full"
             >
               {form.formState.isSubmitting ? "Submitting..." : `${type} Event `}
             </Button>
