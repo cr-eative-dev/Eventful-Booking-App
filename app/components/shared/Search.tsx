537e808533caabda9e110c33f50c95ec1d8f7c26
digitalflowerstudio <floriantappeser@gmail.com>
2024-04-30 14:49:57 +0200

    fix

--- app/components/shared/Search.tsx
+++ app/components/shared/Search.tsx
@@ -40,13 +40,12 @@ const Search = ({
   }, [query, searchParams, router]);
 
   return (
-    <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+    <div className="flex-center gap-2 w-full">
       <SearchIcon />
       <Input
         type="text"
         placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
-        className="p-regular-16 border bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus-visible:ring-0 focus-visible:ring-offset-0"
       />
     </div>
   );
